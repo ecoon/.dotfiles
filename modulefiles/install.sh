a4d8c9b0a051c558880fe541dd8742e1bd85f37e
Ethan Coon <coonet@ornl.gov>
2022-04-22 09:55:23 -0400

    cleanup

--- modulefiles/install.sh
+++ modulefiles/install.sh
@@ -1,4 +1,4 @@
 VER=4.0.5
 
-mkdir -P ~/code/mpi/modulefiles/openmpi
-cp openmpi-$VER ~/code/mpi/modulefiles/openmpi/$VER
+mkdir -P ${CODE_BASE}/mpi/modulefiles/openmpi
+cp openmpi-$VER ${CODE_BASE}/mpi/modulefiles/openmpi/$VER
